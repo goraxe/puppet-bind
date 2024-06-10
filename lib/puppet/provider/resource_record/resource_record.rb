f5fee5e5805a2397a89381ad6d653f1152df105e
goraxe <goraxe@gmail.com>
2024-06-09 19:23:26 -0700

    fix(resource): actually support some form of creation

--- lib/puppet/provider/resource_record/resource_record.rb
+++ lib/puppet/provider/resource_record/resource_record.rb
@@ -4,37 +4,77 @@ require 'puppet/resource_api/simple_provider'
 
 # Implementation for the resource_record type using the Resource API.
 class Puppet::Provider::ResourceRecord::ResourceRecord < Puppet::ResourceApi::SimpleProvider
-  def get(context)
-    context.debug('Returning pre-canned example data')
-    [
-      {
-        name: 'foo',
-        ensure: 'present',
-      },
-      {
-        name: 'bar',
-        ensure: 'present',
-      },
-    ]
+  def get(context, name)
+    context.notice("Getting '#{name}' with context: #{context.inspect}")
+    # use dig to query dns
+    cmd = "dig +noall +answer @localhost #{context[:zone]} #{context[:record]} #{context[:type]}"
+    IO.popen(cmd, 'r+') do |io|
+      result = io.read
+      context.notice(result)
+      # parts has form <zone> <ttl> <class> <type> <data>
+      (zone, ttl, dnsclass, type, data) = result.split("\t")
+    end
+
+      { :zone => zone, :ttl => ttl, :class => dnsclass, :type => type, :data => data }
   end
 
   def create(context, name, should)
-    context.notice("Creating '#{name}' with #{should.inspect}")
+    context.notice("Creating '#{name}' with #{should.inspect}, context: #{context.inspect}")
+    # pipe commands to nsupdate to update the zone
+     zone = should[:zone]
+     record = should[:record]
+     type = should[:type]
+     data = should[:data]
+     ttl = should[:ttl]
+     cmd = "update add #{record}.#{zone} #{ttl} #{type} #{data}\nsend"
+     IO.popen('nsupdate', 'r+') do |io|
+       io.puts(cmd)
+       io.close_write
+       context.notice(io.read)
+     end
   end
 
   def update(context, name, should)
-    context.notice("Updating '#{name}' with #{should.inspect}")
+    context.notice("Updating '#{name}' with #{should.inspect}, context: #{context.inspect}")
+    zone = should[:zone]
+    record = should[:record]
+    type = should[:type]
+    data = should[:data]
+    ttl = should[:ttl]
+    cmd = "update delete #{record}.#{zone} #{ttl} #{type} #{data}\nsend"
+    IO.popen('nsupdate', 'r+') do |io|
+      io.puts(cmd)
+      io.close_write
+      context.notice(io.read)
+      cmd = "update add #{record}.#{zone} #{ttl} #{type} #{data}\nsend"
+      io.puts(cmd)
+      io.close_write
+      context.notice(io.read)
+    end
   end
 
   def delete(context, name)
-    context.notice("Deleting '#{name}'")
+    context.notice("Deleting '#{name}', context: #{context.inspect}")
+    zone = should[:zone]
+    record = should[:record]
+    type = should[:type]
+    data = should[:data]
+    ttl = should[:ttl]
+    cmd = "update delete #{record}.#{zone} #{ttl} #{type} #{data}\nsend"
+    IO.popen('nsupdate', 'r+') do |io|
+      io.puts(cmd)
+      io.close_write
+      context.notice(io.read)
+    end
   end
 
   def canonicalize(_context, resources)
     resources.each do |r|
-      r[:record] = r[:record].downcase
-      r[:zone] = r[:zone].downcase
+      r[:record] = r[:record]
+      r[:zone] = r[:zone]
       r[:type] = r[:type].upcase
+      r[:data] = r[:data]
+      r[:ttl] = r[:ttl].to_i || 3600
     end
   end
 end
